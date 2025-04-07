a84574ac0ac7b60e2478cbc6764d66ae0958e4d7
KAMELALOUI <115537905+KAMELALOUI@users.noreply.github.com>
2025-04-07 18:59:31 +0100

    Update auth.service.ts

--- front/src/app/api/auth.service.ts
+++ front/src/app/api/auth.service.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:8088/api/auth';
+  private apiUrl = 'http://51.20.60.69:8088/api/auth';
 
   constructor(private http: HttpClient, private router: Router) { }
 
